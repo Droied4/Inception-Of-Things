446e5a85bbfe49e92a235be90d97f72b51f7d9de
Droied <droied.dev@gmail.com>
2025-12-16 14:52:26 +0100

    controller and agent mode install

--- p1/server/setup.sh
+++ p1/server/setup.sh
@@ -1,19 +1,58 @@
 #!/bin/bash
 set -e 
 
+CONTROLLER_IP="192.168.56.110"
+TOKEN_FILE="/vagrant/k3s_token"
+
 k3_installation()
 {
-	echo "Installing K3..."
+	echo "Installing dependencies..."
 	sudo apt-get update
 	sudo apt-get install -y curl 
 	ufw disable
-	
+}
+
+install_controller()
+{
+	export K3S_NODE_IP=192.168.56.110
+	echo "Installing k3s controller..."
 	curl -sfL https://get.k3s.io/ | sh
+	
+	echo "Waiting for token to be created..."
+	while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
+		sleep 2
+	done
+
+	echo "Saving token to shared folder..."
+	sudo cat /var/lib/rancher/k3s/server/node-token > "$TOKEN_FILE"
+}
+
+install_agent()
+{
+	export K3S_NODE_IP=192.168.56.111
+	echo "Waiting for controller token..."
+    while [ ! -f "$TOKEN_FILE" ]; do
+        sleep 2
+    done
+
+    TOKEN=$(cat "$TOKEN_FILE")
+
+	echo "Installing k3s agent..."
+    curl -sfL https://get.k3s.io | \
+        K3S_URL="https://${CONTROLLER_IP}:6443" \
+        K3S_TOKEN="$TOKEN" \
+        sh -
 }
 
 main()
 {
+	ROLE="$1"
 	k3_installation
+	if [ "$ROLE" = "controller" ]; then
+		install_controller
+	elif [ "$ROLE" = "agent" ]; then
+		install_agent 
+	fi
 }
 
-main
+main "$@"
