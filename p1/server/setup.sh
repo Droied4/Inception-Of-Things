2a064709519dc398bd0fff587abebeec4c479610
Droied <droied.dev@gmail.com>
2026-01-09 20:59:13 +0100

    p1 finish

--- p1/server/setup.sh
+++ p1/server/setup.sh
@@ -10,13 +10,18 @@ k3_installation()
 	sudo apt-get update
 	sudo apt-get install -y curl 
 	ufw disable
+
+	echo "alias k='kubectl'" >> ~/.bashrc
+	source ~/.bashrc
 }
 
 install_controller()
 {
 	export K3S_NODE_IP=192.168.56.110
 	echo "Installing k3s controller..."
-	curl -sfL https://get.k3s.io/ | sh
+	curl -sfL https://get.k3s.io/ | sh -s - \
+		--node-ip 192.168.56.110 \
+		--write-kubeconfig-mode 644
 	
 	echo "Waiting for token to be created..."
 	while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
@@ -25,6 +30,7 @@ install_controller()
 
 	echo "Saving token to shared folder..."
 	sudo cat /var/lib/rancher/k3s/server/node-token > "$TOKEN_FILE"
+	alias k=kubectl
 }
 
 install_agent()
@@ -32,6 +38,7 @@ install_agent()
 	export K3S_NODE_IP=192.168.56.111
 	echo "Waiting for controller token..."
     while [ ! -f "$TOKEN_FILE" ]; do
+		echo "Waiting for Token file"
         sleep 2
     done
 
@@ -41,7 +48,8 @@ install_agent()
     curl -sfL https://get.k3s.io | \
         K3S_URL="https://${CONTROLLER_IP}:6443" \
         K3S_TOKEN="$TOKEN" \
-        sh -
+        sh -s - \
+		--node-ip 192.168.56.111
 }
 
 main()
