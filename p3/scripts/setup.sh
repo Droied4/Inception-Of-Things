12ee6ed4702d8b8de3298dc3f4faed5c439e3eaa
Droied <droied.dev@gmail.com>
2026-01-14 16:48:59 +0100

    config permissions

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -5,9 +5,19 @@ install_deps()
 {
 	echo "Installing dependencies..."
 	sudo apt-get update
-	sudo apt-get install -y curl 
-    sudo apt-get install docker
+	sudo apt-get install -y curl
+	sudo apt-get install -y docker.io
 	curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+}
+
+config_deps()
+{
+	sudo chmod +x kubectl
+	sudo mv kubectl /usr/local/bin/
+
+	sudo usermod -aG docker $USER
+	newgrp docker
 
 	echo "alias k='kubectl'" >> .bashrc
 	source .bashrc
@@ -16,6 +26,7 @@ install_deps()
 main()
 {
 	install_deps	
+	config_deps
 }
 
 main "$@"
